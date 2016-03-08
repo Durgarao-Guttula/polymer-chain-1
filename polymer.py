07ccfd7329b9037d0d2fcae7f8590c9f490e01c8
Sebastiaan Lokhorst <sebastiaanlokhorst@gmail.com>
2016-03-08 01:12:59 +0100

    Keep track of head-to-tail distance. Implement PERM algorithm (not working properly yet).

--- polymer.py
+++ polymer.py
@@ -1,6 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from numba import jit
+from collections import defaultdict
+
+epsilon = 0.25
+sigma = 0.8
+sigma6 = sigma**6
+sigma12 = sigma**12
+
+T = 1
+N = 100
+
+PERM = False
+
+up_lim = 2
+low_lim = 1.2
+
+R2s = defaultdict(list)
 
 @jit
 def lennard_jones(r):
@@ -9,63 +25,89 @@ def lennard_jones(r):
 	r8 = r6*r2
 	r12 = r6*r6
 	r14 = r8*r6
-	Fij = r * 4 * ( 12*(1/r14) -6*(1/r8) )
-	Vij = 4 * ( (1/r12) - (1/r6) )
-	return Fij, Vij
+	Vij = 4 * epsilon * ( (sigma12/r12) - (sigma6/r6) )
+	return Vij
 
-def E(beads, new_bead):
+def E(polymer, new_bead):
 	Vj = 0
-	for bead in beads:
-		Fij, Vij = lennard_jones(new_bead-bead)
+	for bead in polymer:
+		Vij = lennard_jones(new_bead-bead)
 		Vj += Vij
 	return Vj
 
-def plot_beads(beads):
-	b_ary = np.array(beads)
+def plot_polymer(polymer):
+	b_ary = np.array(polymer)
 	plt.plot(b_ary[:,0], b_ary[:,1], 'o-')
+	plt.axis('equal')
 	plt.title('Polymer chain')
 	plt.grid(True)
 	plt.show()
 
-
-def add_bead(beads, T):
+def add_bead(polymer, pol_weight, L):
 	angles = np.arange(0, 2*np.pi, 2/6*np.pi).reshape(6, 1) + (2/6*np.pi)*np.random.rand()
 	delta_pos = np.concatenate((np.cos(angles), np.sin(angles)), axis=1)
-	new_pos = beads[-1] + delta_pos
+	new_pos = polymer[-1] + delta_pos
 	w_l = np.ndarray(6)
 	for j in range(0,6):
-		w_l[j] = np.exp(-E(beads,new_pos[j])/T)
-	#debug
-	if(np.count_nonzero(w_l)==0):
-		print(w_l)
-		for j in range(0,6):
-			print('E=',E(beads,new_pos[j]))
-			print('w=',np.exp(-E(beads,new_pos[j])/T))
-		b_ary = np.array(beads)
-		plt.plot(b_ary[:,0], b_ary[:,1], 'o-')
-		plt.plot(new_pos[:,0],new_pos[:,1], 'r.')
-		plt.title('All options impossible!')
-		plt.grid(True)
-		plt.show()
-		exit()
-	#end debug
+		w_l[j] = np.exp(-E(polymer,new_pos[j])/T)
+	if np.count_nonzero(w_l) == 0:
+		print("all options impossible")
+		R2s[L].append(np.sum(polymer[-1]*polymer[-1]))
+		# b_ary = np.array(polymer)
+		# plt.plot(b_ary[:,0], b_ary[:,1], 'o-')
+		# plt.plot(new_pos[:,0],new_pos[:,1], 'r.')
+		# plt.axis('equal')
+		# plt.title('All options impossible!')
+		# plt.grid(True)
+		# plt.show()
+		return
 	W_l = np.sum(w_l)
 	p_l = w_l/W_l
 	j = np.random.choice(6, p=p_l)
-	beads.append(new_pos[j])
+	polymer.append(new_pos[j])
+	pol_weight *= w_l[j]
+	if L < N:
+		if PERM and pol_weight > up_lim:
+			new_polymer1 = polymer[:]
+			new_polymer2 = polymer[:]
+			add_bead(new_polymer1, 0.5*pol_weight, L+1)
+			add_bead(new_polymer2, 0.5*pol_weight, L+1)
+		elif PERM and pol_weight < low_lim:
+			if np.random.rand() < 0.5:
+				add_bead(polymer, 2*pol_weight, L+1)
+		else:
+			add_bead(polymer, pol_weight, L+1)
+	else:
+		print("reached maximum length")
+		R2s[L].append(np.sum(polymer[-1]*polymer[-1]))
+		#plot_polymer(polymer)
 
 ################################################
-T = 1
-beads = []
-N = 1000
 
-# IC: first two beads
-beads.append(np.array([0.0, 0.0]))
-beads.append(np.array([1.0, 0.0]))
+num_runs = 1000
+for i in range(0,num_runs):
+	print("run {} of {}".format(i, num_runs))
 
-# run the simulation
-for i in range(0, N):
-	add_bead(beads, T)
+	# IC: first two beads
+	polymer = []
+	polymer.append(np.array([0.0, 0.0]))
+	polymer.append(np.array([1.0, 0.0]))
+	pol_weight = 1
+	L = 2
 
-# display results
-plot_beads(beads)
+	# run the simulation
+	add_bead(polymer, pol_weight, L)
+
+Ls = []
+R2s_mean = []
+R2s_std = []
+for L, R2vals in sorted(R2s.items()):
+	Ls.append(L)
+	R2s_mean.append(np.mean(R2vals))
+	R2s_std.append(np.std(R2vals))
+
+plt.errorbar(Ls, R2s_mean, yerr=R2s_std, fmt='.')
+plt.xlabel('$L$')
+plt.ylabel('$R^2$')
+plt.show()
+
